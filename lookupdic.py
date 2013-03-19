910b0dadf41e51a719e58d1b73484da91412b295
shvva <shvva@symbigan.com>
2013-03-19 12:14:52 +0900

    Added Python3 codes

--- lookupdic.py
+++ lookupdic.py
@@ -1,13 +1,23 @@
 # lookupdic - Look up the words with Multiple Selections by means of alc EIJIRO on the web
 #        a word on the cursor would be refered in case of no selection
 
-import sublime, sublime_plugin, webbrowser, urllib
+import sublime, sublime_plugin, webbrowser,sys
+
+ST3 = sys.version_info >= (3, 0, 0)
+
+if ST3:
+	import urllib.parse
+else:
+	import urllib
 
 # URL to alc EIJIROweb
 EIJIRO_URL = 'http://eow.alc.co.jp/search?q='
 
 def lookupweb(str):
-	webbrowser.open_new_tab(EIJIRO_URL + urllib.quote_plus(str.encode('utf-8', 'ignore')))
+	if ST3:
+		webbrowser.open_new_tab(EIJIRO_URL + urllib.parse.quote_plus(str.encode('utf-8', 'ignore')))
+	else:
+		webbrowser.open_new_tab(EIJIRO_URL + urllib.quote_plus(str.encode('utf-8', 'ignore')))
 
 class lookupdicCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
